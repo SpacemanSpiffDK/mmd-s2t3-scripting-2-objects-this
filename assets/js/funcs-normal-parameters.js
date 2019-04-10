234fb00251882a4dfc267ecc111146b6e50824ec
Dan Høegh <dan.hoegh@gmail.com>
2019-04-10 12:28:41 +0200

    split js in to 3 files

---
+++ assets/js/funcs-normal-parameters.js
@@ -0,0 +1,28 @@
+// JS by Dan Høegh
+// UCN MMD 2019
+
+// normal
+// function parameters - WITH DEFAULT VALUE
+
+function manipulateClass(targetSelector, className, isAdd = true) {
+
+    // default values :::
+    // function manipulateClass(targetSelector, className, isAdd = true)
+    // so if we only pass the first 2 parameters we get isAdd as true
+    // manipulateClass(targetSelector, className)
+
+    let elms = document.querySelectorAll(targetSelector);
+    for (let i = 0; i < elms.length; i++) {
+        if (isAdd) {
+            elms[i].classList.add(className);
+        } else {
+            elms[i].classList.remove(className);
+        }
+    }
+}
+
+window.onload = function () {
+    manipulateClass("#block1", "red", false);
+    manipulateClass(".block2", "blue");
+    manipulateClass("textarea", "green");
+}
